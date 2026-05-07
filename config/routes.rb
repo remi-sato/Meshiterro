583c85c30e7d253a5f9b0348d3cdb1d6dc2e3b8d
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-05-07 14:48:31 +0000

    [Finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   
   get "homes/about",to: "homes#about",as: "about"
 
+  resources :post_image,only: [:new, :index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
---
+++ db/migrate/20260507094409_create_post_images.rb
@@ -0,0 +1,7 @@
+class CreatePostImages < ActiveRecord::Migration[8.0]
+  def change
+    create_table :post_images do |t|
+      t.timestamps
+    end
+  end
+end
