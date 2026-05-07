f58351da03f118cfc31a7cf6578f3798fc386a61
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-05-07 07:17:41 +0000

    [Finish]4-6

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  # 認証をスキップ：サインアップ(new,create)はログイン前に行うため
+  allow_unauthenticated_access only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_session_path, notice: "ユーザー登録が完了しました！続けてログインしてください。"
+    else
+      # エラー時はフォームを再表示
+      render :new,status: :unprocessable_entity
+  end
+end
+
+ private
+
+ def user_params
+   # name,email_address,password,password_confirmationを許可
+   params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
+ end  
+end
