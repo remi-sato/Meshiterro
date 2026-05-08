c4771489c572e64f101fe957c880614083d624a1
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-05-08 02:02:35 +0000

    [Finish]9-10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_07_094409) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_08_014344) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,6 +42,9 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_07_094409) do
   create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
