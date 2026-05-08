a6c469cc5db5e4468bd03e17724ef0f894118587
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-05-08 03:12:18 +0000

    [Finish]4-11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,10 +8,11 @@ class PostImagesController < ApplicationController
     @post_image= PostImage.new(post_image_params)
     @post_image.user_id= Current.user.id
     @post_image.save
-    redirect_to post_image_path(@post_image)
+    redirect_to post_image_path
   end  
 
   def index
+    @post_images=PostImage.all
   end
 
   def show
