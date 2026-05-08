f3ba4023eb24e244c8ec23842fa6633815c2c6d7
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-05-08 09:09:40 +0000

    [Finish]4-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,16 @@ class UsersController < ApplicationController
     @post_images=@user.post_images
   end
 
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    user=User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path
+  end
+
  private
 
  def user_params
